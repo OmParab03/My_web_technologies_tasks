e936937e5579a16feeab2fd1325e6fc4b0458a08
Omkar Parab <omkarparab2110@gmail.com>
2026-01-15 21:06:25 +0530

    update task2

--- experiment-1/task2/test.js
+++ experiment-1/task2/test.js
@@ -12,23 +12,46 @@
 function marks(){
     const mark=document.getElementById("mark")
     let m=mark.value
+    const outputdiv=document.getElementById("outputdiv")
+    if(m){
 
-    if(m>=90){
-        document.writeln("<h1>A grade.😀</h1>")
-    }
-    else if(m<90 & m>=80){
-        document.writeln("<h1>B grade.😄</h1>")
-    }
-    else if(m<80 & m>=70){
-        document.writeln("<h1>C grade.😊</h1>")
-    }
-    else if(m<70 & m>=60){
-        document.writeln("<h1>D grade.😐</h1>")
-    }
-    else if(m<60 & m>=50){
-        document.writeln("<h1>E grade.😑</h1>")
-    }
-    else{
-        document.writeln("<h1>F grade🤧</h1>")
+        if(m>=90){
+            outputdiv.innerHTML="<span><h1>A grade.😀</h1></span>"
+        }
+        else if(m<90 & m>=80){
+            outputdiv.innerHTML="<span><h1>B grade.😄</h1></span>"
+        }
+        else if(m<80 & m>=70){
+            outputdiv.innerHTML="<span><h1>C grade.😊</h1></span>"
+        }
+        else if(m<70 & m>=60){
+            outputdiv.innerHTML="<span><h1>D grade.😐</h1></span>"
+        }
+        else if(m<60 & m>=50){
+            outputdiv.innerHTML="<span><h1>E grade.😑</h1></span>"
+        }
+        else{
+            outputdiv.innerHTML="<span><h1>F grade🤧</h1></span>"
+        }
+    }else{
+        outputdiv.innerHTML="<span><h1>Please enter valid marks.😶‍🌫️</h1></span>"
     }
 }
+
+
+function checkEvenOdd(){
+    const number=document.getElementById("number")
+    let n=number.value
+    const outputdiv=document.getElementById("outputdiv")
+    if(n){
+    if(n%2===0){
+        outputdiv.innerHTML="<span><h1>Even Number.😀</h1></span>"
+
+    }
+    else{
+        outputdiv.innerHTML="<span><h1>Odd Number.😄</h1></span>"
+    }
+    }else{
+        outputdiv.innerHTML="<span><h1>Please enter a valid number.😶‍🌫️</h1></span>"} 
+}
+
